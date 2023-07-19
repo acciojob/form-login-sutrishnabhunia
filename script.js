e7534bfb48d62de729173d9b4d9e4d120228d799
Sutrishna Bhunia <134145442+sutrishnabhunia@users.noreply.github.com>
2023-07-19 12:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
 
+	const FNinput = document.querySelector('input[name="fname"]');
+	const LNinput = document.querySelector('input[name="lname"]');
+
+	const fval = FNinput.value.trim();
+	const lval = LNinput.value.trim();
+	alert(fval+" " +lval);
+
 }
